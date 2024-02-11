0d509c54852abbdfca4462e73424d2d376aea9a6
Roberto Park <betopark97@gmail.com>
2024-02-11 14:20:41 +0900

    change multipage names and work on homepage

--- Home.py
+++ Home.py
@@ -1,1 +1,21 @@
 import streamlit as st
+
+st.set_page_config(
+    page_icon="🧑‍💻",
+    page_title="Roberto's Portfolio"
+)
+
+header = st.container()
+
+info = st.container()
+
+
+with header:
+
+    col1, col2 = st.columns(2, gap="small")
+    
+    with col1:
+        st.image("./images/profile.png", use_column_width="auto")
+    
+    with col2:
+        st.title("Roberto Park")
